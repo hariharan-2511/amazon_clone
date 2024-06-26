7f283b538e952de0a0ff369d0d1b1adf24f1161c
Hari <hariroriri25Gmail.com>
2024-06-26 16:06:48 +0530

    inital commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const imgs = document.querySelectorAll('.header-slider ul img');
+const prev_btn = document.querySelector('.control-prev');
+const next_btn = document.querySelector('.control-next');
+
+let n = 0;
+
+function changeSlide() {
+    for (let i = 0; i < imgs.length; i++) {
+        imgs[i].style.display = 'none'; // Hide all images
+    }
+    imgs[n].style.display = 'block'; // Show the current image
+}
+
+changeSlide(); // Initial call to display the first image
+
+prev_btn.addEventListener('click', () => {
+    if (n > 0) {
+        n--;
+    } else {
+        n = imgs.length - 1; // Wrap around to the last image
+    }
+    changeSlide();
+});
+
+next_btn.addEventListener('click', () => {
+    if (n < imgs.length - 1) {
+        n++;
+    } else {
+        n = 0; // Wrap around to the first image
+    }
+    changeSlide();
+});
