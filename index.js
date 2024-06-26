597f2a708670fc69296839c1aca30e6e44cf6108
Hari <hariroriri25Gmail.com>
2024-06-26 17:43:11 +0530

    overall commit

--- index.js
+++ index.js
@@ -30,3 +30,12 @@ next_btn.addEventListener('click', () => {
     }
     changeSlide();
 });
+
+
+const scrollContainer = document.querySelectorAll('.products');
+for (const item of scrollContainer) {
+    item.addEventListener('wheel', (evt)=>{
+        evt.preventDefault();
+        item.scrollLeft += evt.deltaY
+    });
+}
